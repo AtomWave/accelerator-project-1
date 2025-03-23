68e757e0ab0963847aa2fd8d7d73501a4a83f8dd
AtomWave <78145765+AtomWave@users.noreply.github.com>
2025-03-23 19:28:04 +0300

    Revert "размечает и стилизует блок reviews"
    This reverts commit c81e6f1c221d3d98d71f34ab5ee5c493e3885286.

--- source/js/main.js
+++ source/js/main.js
@@ -6,9 +6,7 @@
 import { initVideoLoader } from './modules/button-play.js';
 import { initTabs } from './modules/tabs.js';
 import { initCoachesSwiper } from './modules/swiper-juri.js';
-import { initReviewsSwiper } from './modules/swiper-reviews.js';
 
 initVideoLoader();
 initTabs();
 initCoachesSwiper();
-initReviewsSwiper();
