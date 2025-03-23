575dd89c1edfe3add3ed526a255a80e955122481
AtomWave <78145765+AtomWave@users.noreply.github.com>
2025-03-23 19:52:11 +0300

    стилизует блок с отзывами

--- source/js/main.js
+++ source/js/main.js
@@ -6,7 +6,9 @@
 import { initVideoLoader } from './modules/button-play.js';
 import { initTabs } from './modules/tabs.js';
 import { initCoachesSwiper } from './modules/swiper-juri.js';
+import { initReviewsSwiper } from './modules/swiper-reviews.js';
 
 initVideoLoader();
 initTabs();
 initCoachesSwiper();
+initReviewsSwiper();
